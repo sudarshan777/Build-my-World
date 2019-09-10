a2117b07c2b401b9227a322f823f5a6a93c2e7e0
Sudarshan Agrawal <sudarshan.shubham@gmail.com>
2019-09-10 14:59:14 -0400

    First commit

---
+++ build/CMakeFiles/welcome_msg.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/welcome_msg.dir/script/welcome_msg.cpp.o"
+  "libwelcome_msg.pdb"
+  "libwelcome_msg.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/welcome_msg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
